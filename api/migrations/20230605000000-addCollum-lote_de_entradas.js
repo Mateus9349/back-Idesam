2c556dbc2d4d221e7b98a10d63de546d9d24b81e
Mateus9349 <mateusvpdl@outlook.com>
2023-06-28 14:20:03 -0400

    possiveis alterações

--- api/migrations/20230605000000-addCollum-lote_de_entradas.js
+++ api/migrations/20230605000000-addCollum-lote_de_entradas.js
@@ -2,13 +2,13 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
     async up(queryInterface, Sequelize) {
-        await queryInterface.addColumn('lote_de_entradas','valor_pago',{
+        await queryInterface.addColumn('Lote_de_entradas','valor_pago',{
                 allowNull: false,
                 type: Sequelize.FLOAT
             }
         );
     },
     async down(queryInterface, Sequelize) {
-        await queryInterface.removeColumn('lote_de_entradas', 'valor_pago');
+        await queryInterface.removeColumn('Lote_de_entradas', 'valor_pago');
     }
 };
--- api/migrations/20230605002023-addCollum-lote_de_entradas.js
+++ api/migrations/20230605002023-addCollum-lote_de_entradas.js
@@ -2,13 +2,13 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
     async up(queryInterface, Sequelize) {
-        await queryInterface.addColumn('lote_de_entradas','ativo',{
+        await queryInterface.addColumn('Lote_de_entradas','ativo',{
                 allowNull: false,
                 type: Sequelize.BOOLEAN
             }
         );
     },
     async down(queryInterface, Sequelize) {
-        await queryInterface.removeColumn('lote_de_entradas', 'ativo');
+        await queryInterface.removeColumn('Lote_de_entradas', 'ativo');
     }
 };
